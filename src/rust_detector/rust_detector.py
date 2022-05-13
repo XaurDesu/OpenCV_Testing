9002ff734db2e47c4ec4f81c363042ec11d398ed
Jaime Andres Torres B <69700780+XaurDesu@users.noreply.github.com>
2022-05-13 17:46:37 -0500

    Update rust_detector.py

--- src/rust_detector/rust_detector.py
+++ src/rust_detector/rust_detector.py
@@ -2,7 +2,7 @@ import cv_bridge
 import rospy
 from sensor_msgs.msg import Image
 from std_msgs.msg import String
-import ros_numpy
+import ros_numpy as np
 import cv2
 
 ROTOPIC_NAME="/robocol/vision/cam_0"
@@ -13,6 +13,7 @@ fontColor              = (255,255,255)
 thickness              = 1
 lineType               = 2
 dim=None
+
 def image_recived(msg):
     print("[INFO]: Image Received, showImage function called")
     # if MOVEMENT_RECIVED:
@@ -35,6 +36,42 @@ def calibrateParameters(img):
     height = int(img.shape[0] * scale_percent / 100)  
     dim = (width, height)
 
+# This function allows us to create a descending sorted list of contour areas.
+def contour_area(contours):
+    cnt_area = []
+ 
+    for i in range(0,len(contours),1):
+        cnt_area.append(cv2.contourArea(contours[i]))
+ 
+    list.sort(cnt_area, reverse=True)
+    return cnt_area
+
+def draw_bounding_boxes(contours, image):
+    cnt_area = contour_area(contours)
+ 
+    for i in range(0,len(contours),1):
+        cnt = contours[i]
+        x,y,w,h = cv2.boundingRect(cnt)         
+        image=cv2.rectangle(image,(x,y),(x+w,y+h),(0,0,255),2)
+ 
+    return image
+
+
+def rust_detector(img):
+        
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    lower = np.array([10, 100, 20])
+    upper = np.array([20, 255, 200])
+
+    mask = cv2.inRange(hsv, lower, upper)
+    result = cv2.bitwise_and(img, img, mask=mask)
+
+    contours,hierarchy = cv2.findContours(mask, 1, 2)
+    bounding = draw_bounding_boxes(contours, img)
+        
+        
+    cv2.imshow("frame", img)
 
 
 if __name__ == '__main__':
